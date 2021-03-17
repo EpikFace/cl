3ba11d0b32b336983518772541c00ebba06c2180
EpikFace <73156442+EpikFace@users.noreply.github.com>
2021-03-17 11:17:30 -0500

    shop improvement
    changed shop buttons to disappear and made them look nicer

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 let pointCount = 0;
 let points = document.getElementById("points");
 let updatePoints = () => {points.innerHTML = pointCount};
+let pointsPerClick = document.getElementById("pointsPerClick");
 let clickValue = 1;
 let multiplier = 1;
+let updateMulti = () => {pointsPerClick.innerHTML = clickValue * multiplier}
 let clickerButton = document.getElementById("buto");
 let shopButtonArray = [];
 let shopButtonArrayInput = para => shopButtonArray.push(para);
 let shopButton = document.getElementById("shopButtonOne");
+let disappear = (para, cost) => {if (pointCount >= cost) {document.getElementById(para).style.display = "none"}};
 
 clickerButton.addEventListener('click', function() {
     pointCount = pointCount + (clickValue * multiplier);
@@ -16,17 +19,17 @@ shopButton.addEventListener("click", function() {
     if (pointCount >= 100 && shopButtonArray.includes("shopButtonNumberOne") === false) {
         multiplier = multiplier * 2;
         pointCount -= 100;
-        document.getElementById("shopButtonOneText").innerHTML = `(2X) Purchased!`
         shopButtonArrayInput("shopButtonNumberOne");
         updatePoints();
+        updateMulti();
     };
 })
 document.getElementById("shopButtonTwo").addEventListener("click", function() {
     if (pointCount >= 200 && shopButtonArray.includes("shopButtonNumberTwo") === false) {
         multiplier = multiplier * 2;
         pointCount -= 200;
-        document.getElementById("shopButtonTwoText").innerHTML = `(2X) Purchased!`
         shopButtonArrayInput("shopButtonNumberTwo");
         updatePoints();
+        updateMulti();
     };
 })
